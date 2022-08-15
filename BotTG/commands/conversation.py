eea3ee17945eccb74d753796eabc779b9b45b264
Ivan <vana.antropov@gmail.com>
2022-08-15 14:22:39 +0500

    Iduno2

--- BotTG/commands/conversation.py
+++ BotTG/commands/conversation.py
@@ -5,12 +5,12 @@ from keyboard import kb_user
 from aiogram.types import ReplyKeyboardRemove
 
 async def send_welcome(message: types.Message):
-    await message.reply("Hi!\nI'm GAY!!!", reply_markup=kb_user)
+    await message.reply("Hi!", reply_markup=kb_user)
     
 async def send_something(message: types.Message):
-    await message.reply("Hi!\nYou're GAY!!!") # reply_makup=ReplyKeyboardRemove()
+    await message.reply("Hi!") # reply_makup=ReplyKeyboardRemove()
     
 def register_handlers_conversation(dp: Dispatcher):
     dp.register_message_handler(send_welcome, commands=['start','help'])
-    dp.register_message_handler(send_something, commands=['who'])
+    dp.register_message_handler(send_something, commands=['hi'])
     
--- BotTG/keyboard/buttons.py
+++ BotTG/keyboard/buttons.py
@@ -3,18 +3,18 @@ from aiogram import types, Dispatcher
 from barter import dp, bot
 
 
-inline_btn_1 = InlineKeyboardButton('Первая кнопка!', callback_data='button1')
+inline_btn_1 = InlineKeyboardButton('кнопка', callback_data='button1')
 inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
 kb = ReplyKeyboardMarkup()
 
 async def process_command_1(message: types.Message):
-    await message.reply("Первая инлайн кнопка", reply_markup=inline_kb1)
+    await message.reply("кнопище", reply_markup=inline_kb1)
     
 async def process_callback_button1(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!')
+    await bot.send_message(callback_query.from_user.id, 'кнопка!')
     
-def register_handlers_buttons(dp: Dispatcher):
-    dp.register_message_handler(process_command_1, commands=['1'])
+def register_handlers_buttons(dp: Dispatcher,callback : types.CallbackQuery):
+    dp.register_message_handler(process_command_1, commands=['1'],)
     dp.register_message_handler(process_callback_button1, callback_data == 'button1')
     
